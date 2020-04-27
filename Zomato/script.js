c7af621e9aa6be2dc660bbf9ec24c1f4f64fdc87
Yu Tang Lin <yutang0416@gmail.com>
2020-04-27 14:58:02 -0400

    Add Sort html & Add more searched result

--- Zomato/script.js
+++ Zomato/script.js
@@ -4,18 +4,9 @@ myZomato.apiKey = "ed57e4f690ae84719a513bd21731b370";
 myZomato.torontoCityId = 89;
 myZomato.currentCategoryIndex = 0;
 
-
-
 myZomato.emptySearchResult = function() {
     $(".search-result").empty();
 };
-
-myZomato.setSearchResultHeaders = function() {
-    const htmlAppendHeader = `
-            <h2>Search Result</h2>
-    `;
-    $(".search-result").append(htmlAppendHeader);
-}
 
 myZomato.getColletions = function() {
     const collectionsPromise = $.ajax({
@@ -146,7 +137,6 @@ myZomato.addPagination = function(numberOfRestaurant, limitPerPage) {
 }
 
 myZomato.getCuisines = function(cuisineId) {
-    myZomato.setSearchResultHeaders();
     const cuisinesPromise = $.ajax({
         headers: {"user-key": `${myZomato.apiKey}`},
         url:"https://developers.zomato.com/api/v2.1/search",
@@ -206,7 +196,6 @@ myZomato.getCuisines = function(cuisineId) {
 };
 
 myZomato.searchRestaurant = function(restaurantName) {
-    myZomato.setSearchResultHeaders();
     const restaurantPromise = $.ajax({
         headers: {"user-key": `${myZomato.apiKey}`},
         url:"https://developers.zomato.com/api/v2.1/search",
@@ -299,6 +288,10 @@ myZomato.listeners = function() {
         $("form").trigger("reset");
     });
 
+    $("select").change(function() {
+        
+        // alert($("select option:selected").text());
+    });
 };
 
 myZomato.setCategoryButton = function() {
@@ -344,6 +337,90 @@ myZomato.setCategoryButton = function() {
     });
 };
 
+myZomato.restaurantNearMe = function() {
+    const restaurantPromise = $.ajax({
+        headers: {"user-key": `${myZomato.apiKey}`},
+        url:"https://developers.zomato.com/api/v2.1/search",
+        dataType: 'json',
+        method: 'GET',
+        data: {
+            entity_id: myZomato.torontoCityId,
+            entity_type: "city",
+            start: 0,
+            lat: 43.627499,
+            lon: -79.396167,
+            sort: "real_distance"
+        }
+    });
+    const restaurantPromise2 = $.ajax({
+        headers: {"user-key": `${myZomato.apiKey}`},
+        url:"https://developers.zomato.com/api/v2.1/search",
+        dataType: 'json',
+        method: 'GET',
+        data: {
+            entity_id: myZomato.torontoCityId,
+            entity_type: "city",
+            start: 21,
+            lat: 43.627499,
+            lon: -79.396167,
+            sort: "real_distance"
+        }
+    });
+
+
+    $.when(restaurantPromise, restaurantPromise2)
+        .done(function(result, result2) {
+
+            const numberOfResult = result[0].results_shown + result2[0].results_shown;
+
+            let ratingColorIndex = 1;
+            myZomato.addResult(result, ratingColorIndex);
+            ratingColorIndex+=20;
+            myZomato.addResult(result2, ratingColorIndex);
+            let limitPerPage = 10;
+            if(numberOfResult > limitPerPage) {
+                myZomato.addPagination(numberOfResult, limitPerPage);
+            };
+        }).fail(function(error) {
+        console.log(error);
+    });
+
+}
+
+myZomato.addResult = function(result, ratingColorIndex) {
+    const restaurantArray = result[0].restaurants;
+    restaurantArray.forEach(restaurant => {
+        const name = restaurant.restaurant.name;
+        const address = restaurant.restaurant.location.address;
+        const phoneNumber = restaurant.restaurant.phone_numbers;
+        const rating = restaurant.restaurant.user_rating.aggregate_rating;
+        const ratingColor = restaurant.restaurant.user_rating.rating_color;
+        const photo = restaurant.restaurant.featured_image;
+        const url = restaurant.restaurant.url;
+
+        const htmlToAppend = `
+            <a href="${url}" class="restaurant-link">
+                <div class="restaurant-container">
+                    <img src="${photo}" alt="restaurant image" class="restaurant-image">
+                    
+                    <div class="restaurant-description">
+                        <h3>${name}</h3>
+                        <p>Address: ${address}</p>
+                        <p>Phone: ${phoneNumber}</p>
+                    </div>
+                    <div class="rating rating${ratingColorIndex}">
+                        <p>${rating}</p>
+                    </div>
+                </div>
+            </a>
+        `;
+
+        $(".search-result").append(htmlToAppend);
+        $(`.rating${ratingColorIndex} p`).css("background-color", `#${ratingColor}`);
+        ratingColorIndex++;
+    });
+};
+
 myZomato.resetCategoryScrollView = function() {
     $(".category-list").scrollLeft(0);
 }
@@ -351,6 +428,7 @@ myZomato.resetCategoryScrollView = function() {
 myZomato.init = function() {
     myZomato.getColletions();
     myZomato.resetCategoryScrollView();
+    myZomato.restaurantNearMe();
     myZomato.setCategoryButton();
     myZomato.listeners();
 };
